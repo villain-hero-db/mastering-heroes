52ea91929b5dee0ed5c5cfa4dd26b7c2a7427575
davidvrl <veral.david@gmail.com>
2020-02-18 18:58:47 +0100

    funcionando

--- models/user.model.js
+++ models/user.model.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
 const userSchema = new Schema({
   username: String,
   password: String,
   teams: Array,
   favourites: Array,
-  avatarPath: String
+  avatarPath: {
+    type: String,
+    default: "https://www.superherodb.com/pictures2/portraits/10/100/10255.jpg"
+  }
 }, {
   timestamps: {
     createdAt: 'created_at',
     updatedAt: 'updated_at'
   }
 });
-
 const User = mongoose.model('User', userSchema);
 module.exports = User;
