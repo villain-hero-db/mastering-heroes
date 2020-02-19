5f17d908b806149b623f0d7610bca5d461fd83cf
davidvrl <veral.david@gmail.com>
2020-02-19 12:29:36 +0100

    remote

--- configs/mongoose.config.js
+++ configs/mongoose.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require("dotenv").config()
 mongoose
-  .connect(process.env.DB_LOCAL, {
+  .connect(process.env.DB_REMOTE, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
