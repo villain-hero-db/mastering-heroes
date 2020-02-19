8971743b3e35854019d2aad0d5b56e5d875b8610
davidvrl <veral.david@gmail.com>
2020-02-19 18:21:19 +0100

    dynamic search

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,62 @@
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
+
+  // document.getElementById('btn-upload').addEventListener('click', function (event) {
+
+  //   document.querySelector('.file-upload').classList.toggle('visibility')
+
+  // });
+
+  // document.getElementById('favourite').addEventListener('click', function (event) {
+  //   event.preventDefault();
+  //   addtoFavourites(document.getElementById('favourite').value)
+
+  // });
+
+
+  document.getElementById("index-input").addEventListener('keyup', () => {
+    console.log("holaaaa")
+    const input = document.getElementById("index-input").value
+    console.log(input)
+    axios.post("/api", { input })
+      .then(response => {
+
+        let allCharacters = response.data;
+        console.log(allCharacters)
+        document.querySelector('.characters-container').innerHTML = ""
+
+        allCharacters.forEach(element => {
+          const card =
+            `<div class="col-md-3">
+            <div class="card p-3" style="width: 18rem;">
+              <img class="card-img-top" src="${element.image}" alt="${element.name}">
+                <div class=" card-body">
+                  <h5 class="card-title">${element.name}</h5>
+
+                  <a href="/heroes/details/${element.idBD}" class="btn btn-primary">View more</a>
+                </div>
+            </div></div>`
+
+
+
+
+          // `<div class="character-info">
+          //   <div class="name">${element.name}</div>
+          //   <div class="occupation">${element.image}</div>`
+
+          document.querySelector('.characters-container').innerHTML += card
+        });
+
+      })
+      .catch(err => console.log(err))
+
+
+  })
+
+
 
 }, false);
 
@@ -19,8 +75,4 @@ $(function () {
 
 });
 
-document.getElementById('favourite').addEventListener('click', function (event) {
-  event.preventDefault();
-  addtoFavourites(document.getElementById('favourite').value)
 
-});
